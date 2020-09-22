063e14d77ecdc7d15352b0ef78acdc799e2ff67e
xiang <xiang13225080@163.com>
2020-09-22 14:17:13 +0800

    minor update

--- setup.go
+++ setup.go
@@ -294,11 +294,11 @@ func ParseStanza(c *caddy.Controller) (*Kubernetes, error) {
 				chaos random inner busybox.busybox-2 busybox.busybox-3
 			*/
 			args := c.RemainingArgs()
-			fmt.Println("args", args)
-			if len(args) == 1 {
-				argsArr := strings.Split(args[0], " ")
-				for i := 2; i < len(argsArr); i++ {
-					items := strings.SplitN(argsArr[i], ".", 2)
+			fmt.Println("args", args, "len", len(args))
+			if len(args) >= 3 {
+				//argsArr := strings.Split(args[0], " ")
+				for i := 2; i < len(args); i++ {
+					items := strings.SplitN(args[i], ".", 2)
 					if len(items) != 2 {
 						fmt.Println("items len is not 2")
 						return nil, c.ArgErr()
@@ -310,8 +310,8 @@ func ParseStanza(c *caddy.Controller) (*Kubernetes, error) {
 					k8s.podMap[items[0]][items[1]] = &PodInfo{
 						Namespace: items[0],
 						Name:      items[1],
-						Action:    argsArr[0],
-						Scope:     argsArr[1],
+						Action:    args[0],
+						Scope:     args[1],
 					}
 				}
 			}
